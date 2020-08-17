2c79e688ce6b98224b3bf62ab152b90b4af9a52a
Mussie araya <mussie@ualberta.ca>
2020-08-17 23:45:28 +0000

    returning the head function

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('Assertion passed: ' + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log('assertion failed: ' + actual + " !== " + expected);
+  }
+
+
+};
+
+const head = function(array){
+return array[0]
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
