31b34ef9b68fc0b311adbf5bdf3c9c5f8cae5e0c
Mussie araya <mussie@ualberta.ca>
2020-08-20 17:12:18 +0000

    made new fixes

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,17 @@
 
-const eqArrays = function(a ,b){
-  
-  for (var i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
+const eqArrays = function(a ,b) {
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
   }
-  return true;  
-  
 };
 
 
 
-console.log(eqArrays([1, 2, 3], [3, 2, 1]))
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
